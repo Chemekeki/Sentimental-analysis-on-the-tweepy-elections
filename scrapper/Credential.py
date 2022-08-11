eb4000e604044837f1b4762032499e0b98701a9b
Evans Kibet <96278762+Chemekeki@users.noreply.github.com>
2022-08-11 15:11:23 +0300

    Create Credential.py
    This file contains the credentials needed for twitter API

---
+++ scrapper/Credential.py
@@ -0,0 +1,7 @@
+#CREDENTIALS
+# This Credentials are goten from twitter who give the credentials for developer accounts
+access_token = ""
+access_token_secret = ""
+consumer_key = ""
+consumer_secret = ""
+bearer_token = ''
